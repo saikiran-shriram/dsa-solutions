583ccedfde9bdbfa31f0c2e46659fc7d0a1c3df9
Saikiran Shriram <saikiranshriram7116@gmail.com>
2026-07-21 08:43:58 +0530

    Add files via upload

--- Array.py
+++ Array.py
@@ -70,3 +70,20 @@ class Solution(object):
         :rtype: bool
         """
         return n%2 == 0
+
+# Palindrome Number 
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x < 0 or (x % 10 == 0 and x ):
+            return False
+        rev = 0
+        while x> rev :
+            rev = rev * 10 + x%10
+            x = x//10
+        return rev == x or rev // 10 == x
+       
+                
