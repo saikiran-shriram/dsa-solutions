aa8281ad49e870b5a9368cd45877616811ff0ade
Saikiran Shriram <saikiranshriram7116@gmail.com>
2026-08-09 09:31:01 +0530

    Time: 953 ms (20.19%) | Memory: 18.3 MB (58.32%) - LeetSync

---
+++ 15-3sum/3sum.py
@@ -0,0 +1,36 @@
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        result = []
+        nums.sort()
+        for i in range(len(nums)-1) :
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            left,right = i+1,len(nums) -1
+            while left <right :
+                if nums[i]+ nums[left]+ nums[right] < 0 :
+                    left += 1
+                elif nums[i]+nums[left]+nums[right] > 0 :
+                    right -= 1
+                else :
+                    result.append([nums[i],nums[left],nums[right]])
+                    left += 1
+                    right -= 1
+                    while left < right and nums[left] == nums[left-1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right+1]:
+                        right -= 1
+        return result
+
+        # for i in range(len(nums)-2) :
+        #     for j in range(i,len(nums)-1) :
+        #         for k in range(j,len(nums)) :
+        #             if  nums[i] + nums[j] + nums[k] == 0 :
+        #                 result.append([nums[i],nums[j],nums[k]])
+        # return result
+
+
+        
