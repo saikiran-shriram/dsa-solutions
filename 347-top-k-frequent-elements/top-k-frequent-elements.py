93e3ab376a393d366716206a1255597777bf8399
Saikiran Shriram <saikiranshriram7116@gmail.com>
2026-08-19 23:28:12 +0530

    Time: 366 ms (5.27%) | Memory: 22.9 MB (60.14%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -0,0 +1,25 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        result = [None] * k
+        count = {}
+        visited = []
+        for i in range(len(nums)) :
+            if nums[i] in visited :
+                count[nums[i]] += 1
+            else :
+                visited.append(nums[i])
+                count[nums[i]] = 1
+        for i in range(k) :
+            max_key = next(iter(count))
+            max_val = count[max_key]
+            for key in count:
+                if count[key] > max_val:
+                    max_val = count[key]
+                    max_key = key
+            result[i] = max_key
+            del count[max_key]
+        return result
+
+
+        
+        
