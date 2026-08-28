54086d25e2148ac7db1d0583e27254519cc90760
Saikiran Shriram <saikiranshriram7116@gmail.com>
2026-08-28 23:01:16 +0530

    Time: 9 ms (34.92%) | Memory: 22.5 MB (71.44%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -6,11 +6,11 @@ class Solution:
         intervals.sort()
         current = intervals[0]
         for i in range(len(intervals)-1) :
-                if current[1] >= intervals[i+1][0] :
-                    current[1] = max(intervals[i+1][1],current[1])
-                else :
-                    result.append(current)
-                    current = intervals[i+1]
+            if current[1] >= intervals[i+1][0] :
+                current[1] = max(intervals[i+1][1],current[1])
+            else :
+                result.append(current)
+                current = intervals[i+1]
         result.append(current)
         return result
 
