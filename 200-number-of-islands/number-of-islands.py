03950dc3b18a756a76e85cb78695eac1b0087346
Saikiran Shriram <saikiranshriram7116@gmail.com>
2026-09-09 08:12:45 +0530

    Time: 270 ms (17.54%) | Memory: 31 MB (12.01%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.py
@@ -0,0 +1,26 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        visited = set()
+        def fun(i,j,grid) :
+            if i <0 or i>= rows or j <0 or j>= cols :
+                return
+            if grid[i][j] == '0' :
+                return
+            if (i,j) in visited :
+                return False
+            visited.add((i,j))
+
+            fun(i+1,j,grid)
+            fun(i,j+1,grid)
+            fun(i-1,j,grid)
+            fun(i,j-1,grid)
+            return True
+
+        count = 0
+        rows = len(grid)
+        cols = len(grid[0])
+        for i in range(rows) :
+            for j in range(cols) :
+                if fun(i,j,grid) :
+                    count += 1
+        return count
