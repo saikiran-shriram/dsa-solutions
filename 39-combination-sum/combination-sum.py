340ae33f0e61028c922e8d4a583d8a00fce0dc59
Saikiran Shriram <saikiranshriram7116@gmail.com>
2026-09-19 16:16:28 +0530

    Time: 7 ms (73.85%) | Memory: 19.7 MB (10.17%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,19 +1,18 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         result = []
-        current = []
+        current= []
         total = 0
+
         def fun(start,current,total):
             if total == target :
-                result.append(list(current))
+                result.append(current[:])
                 return
             if total > target :
                 return
             for i in range(start,len(candidates)):
                 current.append(candidates[i])
-                fun(i,current,total + candidates[i])
-                if current :
-                    current.pop()
+                fun(i,current,total+candidates[i])
+                current.pop()
             return result
         return fun(0,current,total)
-
